793b88cad948904456289cc86cf71628c75174f5
Binithaa <113223986+Binithaa@users.noreply.github.com>
2022-09-14 10:33:35 +0530

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,40 +1,16 @@
+class Fibonacci {
+  public static void main(String[] args) {
 
- 
-class Fibonacci{
- 
- 
-    static int Fib_Even_Sum(int N)
-    {
-        if (N <= 0)
-            return 0;
- 
-        int fib[] = new int[2 * N + 1];
-        fib[0] = 0;
-        fib[1] = 1;
- 
-      
-        int s = 0;
- 
-      
-        for (int j = 2; j <= 2 * N; j++) {
-            fib[j] = fib[j - 1] + fib[j - 2];
- 
-         
-            if (j % 2 == 0)
-                s += fib[j];
-        }
- 
-        return s;
-    }
- 
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
 
-    public static void main(String[] args)
-    {
-        int N = 11;
- 
-        
-        System.out.println(
-            "Even sum of fibonacci series till number " + N
-            + " is: " + +Fib_Even_Sum(N));
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
     }
+  }
 }
